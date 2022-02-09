094c61a86385560ee0db4994167651f7fc74bba7
CookieDinner <dwedzikowski@gmail.com>
2022-02-09 23:47:55 +0100

    Built web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,9 +23,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "aa8ddd968467777477f82809f9108d3d",
-"/": "aa8ddd968467777477f82809f9108d3d",
-"main.dart.js": "4df1ec53eb5f82523de051ec06d5557a",
+"index.html": "f37b9442aec6130041ab8af44d48486c",
+"/": "f37b9442aec6130041ab8af44d48486c",
+"main.dart.js": "db3ed9bc8e29f2edbefcf0c49083f87c",
 "manifest.json": "ce8a8ec9eb57b364648c1c3b860047c0",
 "version.json": "a4d73a070c789be8ec8bf92839439c81"
 };
